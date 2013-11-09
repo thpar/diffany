8bb73a6e58fa2c111d68c663e560cc6ef4cab1b7
svlandeg <svlandeg@ugent.be>
2013-11-09 20:12:47 +0100

    Abstract layer to call differential algorithms (test 4 ubu)

--- diffany/src/main/java/be/svlandeg/diffany/algorithms/CalculateDiff.java
+++ diffany/src/main/java/be/svlandeg/diffany/algorithms/CalculateDiff.java
@@ -1,6 +1,28 @@
 package be.svlandeg.diffany.algorithms;
 
-// TODO Sofie
-public class CalculateDiff {
+import be.svlandeg.diffany.concepts.ConditionNetwork;
+import be.svlandeg.diffany.concepts.DifferentialNetwork;
+import be.svlandeg.diffany.concepts.ReferenceNetwork;
+
+/**
+ * This class serves as an abstract layer between a GUI and the actual technical implementations 
+ * of various algorithms that can calculate differential networks.
+ * Depending on the parameters, this class will decide which algorithm to call.
+ * 
+ * @author Sofie Van Landeghem
+ */
+public class CalculateDiff 
+{
+	
+	/**
+	 * Calculate the differential network
+	 * @param reference the reference network
+	 * @param condition a condition-specific network
+	 * @return the differential network between the two
+	 */
+	public DifferentialNetwork calculateDiffNetwork(ReferenceNetwork reference, ConditionNetwork condition)
+	{
+		throw new UnsupportedOperationException();
+	}
 
 }

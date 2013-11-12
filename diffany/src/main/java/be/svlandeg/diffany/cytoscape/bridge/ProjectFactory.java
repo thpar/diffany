1f9101e49f1cfb3609a13dbb6856a28953c2bc8c
thpar <thomas@blueapplelane.be>
2013-11-12 11:20:04 +0100

    Start cyto-diffany bridge

---
+++ diffany/src/main/java/be/svlandeg/diffany/cytoscape/bridge/ProjectFactory.java
@@ -0,0 +1,13 @@
+package be.svlandeg.diffany.cytoscape.bridge;
+
+
+/**
+ * The ProjectFactory takes care of translating the Diffany model into Cytoscape networks
+ * and back. This keeps the algorithm completely independent from any Cytoscape libraries.
+ * 
+ * @author thpar
+ *
+ */
+public class ProjectFactory {
+
+}

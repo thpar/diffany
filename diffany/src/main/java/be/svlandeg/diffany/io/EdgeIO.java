70dc81fc6bd03fcd24ee590a4e681fad3285a664
svlandeg <svlandeg@ugent.be>
2014-01-22 17:51:32 +0100

    edge writing

--- diffany/src/main/java/be/svlandeg/diffany/io/EdgeIO.java
+++ diffany/src/main/java/be/svlandeg/diffany/io/EdgeIO.java
@@ -1,9 +1,12 @@
 package be.svlandeg.diffany.io;
 
+import java.io.IOException;
 import java.text.DecimalFormat;
+import java.util.StringTokenizer;
 
 import be.svlandeg.diffany.concepts.Edge;
 import be.svlandeg.diffany.concepts.EdgeDefinition;
+import be.svlandeg.diffany.concepts.Node;
 
 /**
  * This class allows reading or writing an {@link Edge} from File.
@@ -13,6 +16,10 @@ import be.svlandeg.diffany.concepts.EdgeDefinition;
 public class EdgeIO
 {
 	
+	private static String symmString = "symmetrical";
+	private static String directString = "directed";
+	private static String negatedString = "negated";
+	private static String notnegatedString = "not negated";
 
 	/**
 	 * Get a string representation of an edge.
@@ -28,6 +35,29 @@ public class EdgeIO
 	}
 	
 	/**
+	 * Read an Edge from a tab-delimited String.
+	 * 
+	 * @param s the tab-delimited string containing all parameters of the Edge
+	 * @return the corresponding Edge object
+	 * @throws IOException when an error occurs during parsing
+	 */
+	public Edge readFromTab(String s) throws IOException
+	{
+		StringTokenizer stok = new StringTokenizer(s, "\t");
+		String source = stok.nextToken();
+		String target = stok.nextToken();
+		
+		String defS = "";
+		while (stok.hasMoreTokens())
+		{
+			defS += stok.nextToken() + "\t";
+		}
+		
+		EdgeDefinition def =  readDefinitionToTab(defS);
+		return new Edge(new Node(source), new Node(target), def);
+	}
+	
+	/**
 	 * Get a string representation of an edge definition.
 	 * More specifically, print it as: edge.type - symmetrical - weight - negated.
 	 * 
@@ -36,15 +66,15 @@ public class EdgeIO
 	public static String writeDefinitionToTab(EdgeDefinition def)
 	{
 		DecimalFormat df = new DecimalFormat("#.##");
-		String symm = "symmetrical";
+		String symm = symmString;
 		if (! def.isSymmetrical())
 		{
-			symm = "directed";
+			symm = directString;
 		}
-		String neg = "negated";
+		String neg = negatedString;
 		if (! def.isNegated())
 		{
-			neg = "not negated";
+			neg = notnegatedString;
 		}
 		double weight = def.getWeight();
 		String result = def.getType() + '\t' + symm + '\t' + df.format(weight) + '\t' + neg;
@@ -53,15 +83,39 @@ public class EdgeIO
 	}
 	
 	/**
-	 * Read an Edge from a tab-delimited String.
+	 * Read an EdgeDefinition from a tab-delimited String.
 	 * 
-	 * @param s the tab-delimited string containing all parameters of the Edge
-	 * @return the corresponding Edge object
+	 * @return a string representation of this edge , ready for printing
+	 * @throws IOException when an error occurs during parsing
 	 */
-	public Edge readFromTab(String s)
+	public static EdgeDefinition readDefinitionToTab(String s) throws IOException
 	{
-		//TODO v1.1: implement!
-		throw new UnsupportedOperationException("readFromTab not yet implemented");
+		StringTokenizer stok = new StringTokenizer(s, "\t");
+		String type = stok.nextToken();
+		String symm_string = stok.nextToken();
+		double weight = Double.parseDouble(stok.nextToken());
+		String neg_string = stok.nextToken();
+		
+		boolean symmetrical = true;
+		if (symm_string.equals(directString))
+		{
+			symmetrical = false;
+		}
+		else if (! symm_string.equals(symmString))
+		{
+			throw new IOException("Error reading the symmetry state from the edge :" + symm_string);
+		}
+		
+		boolean negated = true;
+		if (neg_string.equals(notnegatedString))
+		{
+			negated = false;
+		}
+		else if (! neg_string.equals(negatedString))
+		{
+			throw new IOException("Error reading the negation state from the edge :" + neg_string);
+		}
+		return new EdgeDefinition(type, symmetrical, weight, negated);
 	}
 	
 	
--- diffany/src/main/java/be/svlandeg/diffany/io/NetworkIO.java
+++ diffany/src/main/java/be/svlandeg/diffany/io/NetworkIO.java
@@ -1,5 +1,9 @@
 package be.svlandeg.diffany.io;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Set;
 
 import be.svlandeg.diffany.concepts.Edge;
@@ -12,10 +16,11 @@ import be.svlandeg.diffany.concepts.Network;
  */
 public class NetworkIO
 {
-	
+
 	/**
 	 * Get a string representation of all edges in a Network, divided by newlines, with edges in a tabbed format.
 	 * 
+	 * @param n the Network that needs to be written
 	 * @return a string representation of all edges in this network, ready for printing
 	 * @see EdgeIO#writeToTab
 	 */
@@ -29,7 +34,29 @@ public class NetworkIO
 		}
 		return result;
 	}
-	
+
+	/**
+	 * Write the string representation of all edges to a File.
+	 * 
+	 * @param f the output file
+	 * @param n the Network that needs to be written
+	 * @see EdgeIO#writeToTab
+	 * 
+	 * @throws IOException when an error occurs during parsing
+	 */
+	public static void writeNetworkToFile(Network n, File f) throws IOException
+	{
+		BufferedWriter writer = new BufferedWriter(new FileWriter(f));
+		for (Edge e : n.getEdges())
+		{
+			writer.append(EdgeIO.writeToTab(e));
+			writer.newLine();
+			writer.flush();
+		}
+		writer.flush();
+		writer.close();
+	}
+
 	/**
 	 * Read all edges from a file containing one tab-delimited edge per line
 	 * 
@@ -38,10 +65,8 @@ public class NetworkIO
 	 */
 	public static Set<Edge> readEdgesFromFile(String location)
 	{
-		//TODO v1.1: implement!
+		// TODO v1.1: implement!
 		throw new UnsupportedOperationException("readEdgesFromFile not yet implemented");
 	}
-	
-	
 
 }

d75f7317efb86b9147b33f0ddf713ff7b10697ed
svlandeg <svlandeg@ugent.be>
2014-04-02 09:14:48 +0200

    header in R script

--- diffany/src/main/resources/Rcode/NormalizeAffyData.R
+++ diffany/src/main/resources/Rcode/NormalizeAffyData.R
@@ -1,3 +1,8 @@
+#source("http://bioconductor.org/biocLite.R");
+#biocLite("affy");
+
+library(affy);
+
 expressionSet <- rma(rawProbeData);
 probesets <- featureNames(expressionSet);
 samples <- sampleNames(expressionSet);

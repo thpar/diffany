fb129f3782aed180e567c67ea503dfaa1043ce06
svlandeg <svlandeg@ugent.be>
2014-08-21 14:08:52 +0200

    testing fuzzy networks with min/max and both overlap and differential

---
+++ diffany/src/main/java/be/svlandeg/diffany/examples/FuzzyNetworks2.java
@@ -0,0 +1,198 @@
+package be.svlandeg.diffany.examples;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import be.svlandeg.diffany.core.algorithms.CalculateDiff;
+import be.svlandeg.diffany.core.networks.Condition;
+import be.svlandeg.diffany.core.networks.ConditionNetwork;
+import be.svlandeg.diffany.core.networks.Edge;
+import be.svlandeg.diffany.core.networks.Node;
+import be.svlandeg.diffany.core.networks.ReferenceNetwork;
+import be.svlandeg.diffany.core.project.LogEntry;
+import be.svlandeg.diffany.core.project.Logger;
+import be.svlandeg.diffany.core.project.Project;
+import be.svlandeg.diffany.core.semantics.DefaultEdgeOntology;
+import be.svlandeg.diffany.core.semantics.DefaultNodeMapper;
+import be.svlandeg.diffany.core.semantics.NodeMapper;
+import be.svlandeg.diffany.core.semantics.TreeEdgeOntology;
+
+/** 
+ * This class provides examples to benchmark the fuzzy overlap & differential functionality.  
+ * Specifically, we use the same interaction type for all edges, but with different weights and complex differences between various cutoffs.
+ * 
+ * @author Sofie Van Landeghem
+ */
+public class FuzzyNetworks2 extends GenericExample
+{
+
+	private NodeMapper nm;
+	
+	/**
+	 * Constructor: generates a default {@link NodeMapper} object
+	 */
+	public FuzzyNetworks2()
+	{
+		nm = new DefaultNodeMapper();
+	}
+	
+	/**
+	 * Get a custom project.
+	 * @return an example project illustrating figure 1C.
+	 */
+	public Project getProject()
+	{
+		String name = "FuzzyNetworks2";
+		TreeEdgeOntology eo = new DefaultEdgeOntology();
+		Project p = new Project(name, eo, nm);
+		return p;
+	}
+	
+	/**
+	 * Add some custom-defined networks to the project: 1 reference network and 3 condition-specific.
+	 * @param p the fuzzy project
+	 * @param overlapCutoff the cutoff for overlap %
+	 * @return the resulting configuration ID.
+	 */
+	public int getTestConfiguration(Project p, int overlapCutoff)
+	{
+		ReferenceNetwork r = getReference();
+		Set<ConditionNetwork> c = new HashSet<ConditionNetwork>();
+		c.add(getCondition1());
+		c.add(getCondition2());
+		c.add(getCondition3());	
+		int ID = p.addRunConfiguration(r, c, overlapCutoff);
+		return ID;
+	}
+
+
+	/**
+	 * Get the reference network useful for testing
+	 * @return the reference network
+	 */
+	private ReferenceNetwork getReference()
+	{
+		Map<String, Node> nodes = new HashMap<String, Node>();
+		nodes.put("A", new Node("A"));
+		nodes.put("B", new Node("B"));
+		
+		nodes.put("X", new Node("X"));
+		nodes.put("Y", new Node("Y"));
+		
+		ReferenceNetwork network = new ReferenceNetwork("Fuzzy2 reference network", 1, nm);
+		
+		network.addEdge(new Edge("ppi", nodes.get("A"), nodes.get("B"), true, 0.8, false));
+		
+		return network;
+	}
+	
+	/**
+	 * Get the first condition-specific network
+	 * @return the first condition-specific network
+	 */
+	private ConditionNetwork getCondition1()
+	{
+		String description = "treated with MMS 1";
+		Condition c = new Condition(description);
+		Set<Condition> conditions = new HashSet<Condition>();
+		conditions.add(c);
+
+		ConditionNetwork network = new ConditionNetwork("Condition network 1", 11, conditions, nm);
+
+		Map<String, Node> nodes = new HashMap<String, Node>();
+		nodes.put("A", new Node("A"));
+		nodes.put("B", new Node("B"));
+		
+		nodes.put("X", new Node("X"));
+		nodes.put("Y", new Node("Y"));
+		
+		network.addEdge(new Edge("ppi", nodes.get("A"), nodes.get("B"), true, 1.2, false));
+
+		return network;
+	}
+
+	/**
+	 * Get the second condition-specific network
+	 * @return the second condition-specific network
+	 */
+	private ConditionNetwork getCondition2()
+	{
+		String description = "treated with MMS 2";
+		Condition c = new Condition(description);
+		Set<Condition> conditions = new HashSet<Condition>();
+		conditions.add(c);
+
+		ConditionNetwork network = new ConditionNetwork("Condition network 2", 12, conditions, nm);
+
+		Map<String, Node> nodes = new HashMap<String, Node>();
+		nodes.put("A", new Node("A"));
+		nodes.put("B", new Node("B"));
+		
+		nodes.put("X", new Node("X"));
+		nodes.put("Y", new Node("Y"));
+		
+		network.addEdge(new Edge("ppi", nodes.get("A"), nodes.get("B"), true, 0.6, false));
+
+		return network;
+	}
+	
+	/**
+	 * Get the third condition-specific network
+	 * @return the third condition-specific network
+	 */
+	private ConditionNetwork getCondition3()
+	{
+		String description = "treated with MMS 3";
+		Condition c = new Condition(description);
+		Set<Condition> conditions = new HashSet<Condition>();
+		conditions.add(c);
+
+		ConditionNetwork network = new ConditionNetwork("Condition network 3", 13, conditions, nm);
+
+		Map<String, Node> nodes = new HashMap<String, Node>();
+		nodes.put("A", new Node("A"));
+		nodes.put("B", new Node("B"));
+		
+		nodes.put("X", new Node("X"));
+		nodes.put("Y", new Node("Y"));
+		
+		network.addEdge(new Edge("ppi", nodes.get("B"), nodes.get("A"), true, 0.4, false));
+
+		return network;
+	}
+	
+	
+	/**
+	 * Testing the example using console output (use TestFuzzyOverlap for the JUnit version!)
+	 * @param args (ignored) argument list
+	 */
+	public static void main(String[] args)
+	{
+		FuzzyNetworks2 ex = new FuzzyNetworks2();
+		double weight_cutoff = 0.0;
+		
+		System.out.println("Defining network for FuzzyNetworks2 configuration");
+		Project p = ex.getProject();
+		int overlap_cutoff = 3;
+		int ID = ex.getTestConfiguration(p, overlap_cutoff);
+		
+		System.out.print("Calculating 1-all differential networks at weight cutoff " + weight_cutoff);
+		System.out.println(" and overlap cutoff " + overlap_cutoff);
+		
+		boolean minOperator = true;
+		new CalculateDiff().calculateOneDifferentialNetwork(p, ID, weight_cutoff, 70, 80, minOperator);	
+		
+		System.out.println("");
+		ex.printAllNetworks(p, ID, true, false, false);	
+		
+		System.out.println("Log:");
+		Logger logger = p.getLogger(ID);
+		for (LogEntry log : logger.getAllLogMessages())
+		{
+			System.out.println(log);
+		}
+	}
+	
+}
---
+++ diffany/src/test/java/be/svlandeg/diffany/junit/TestFuzzyBoth.java
@@ -0,0 +1,219 @@
+package be.svlandeg.diffany.junit;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Set;
+
+import org.junit.Test;
+
+import be.svlandeg.diffany.core.algorithms.CalculateDiff;
+import be.svlandeg.diffany.core.networks.DifferentialNetwork;
+import be.svlandeg.diffany.core.networks.Edge;
+import be.svlandeg.diffany.core.networks.OutputNetworkPair;
+import be.svlandeg.diffany.core.networks.OverlappingNetwork;
+import be.svlandeg.diffany.core.project.Project;
+import be.svlandeg.diffany.core.project.RunOutput;
+import be.svlandeg.diffany.examples.FuzzyNetworks2;
+
+/** 
+ * This class provides examples to benchmark the fuzzy overlap & differential functionality, varying overlap cutoffs and min/max operators.  
+ * 
+ * @author Sofie Van Landeghem
+ */
+public class TestFuzzyBoth extends TestGeneric
+{
+
+	/**
+	 * JUNIT Test: check whether the example fuzzy network produces correct results with the min operator and cutoff 4 out of 4
+	 */
+	@Test
+	public void testFuzzy_4_min()
+	{
+		FuzzyNetworks2 ex = new FuzzyNetworks2();
+		double weight_cutoff = 0.0;
+		Project p = ex.getProject();
+		int overlap_cutoff = 4;
+		int ID = ex.getTestConfiguration(p, overlap_cutoff);
+
+		new CalculateDiff().calculateOneDifferentialNetwork(p, ID, weight_cutoff, 70, 80, true);
+
+		// Testing that there is exactly one differential network created
+		RunOutput output = p.getOutput(ID);
+		assertNrPairs(output, 1);
+		
+		OutputNetworkPair pair = output.getOutputAsPairs().iterator().next();
+
+		// Testing the edges in the differential network
+		DifferentialNetwork dn = pair.getDifferentialNetwork();
+		Set<Edge> dEdges = dn.getEdges();
+		assertEquals(0, dEdges.size());
+		
+		// Testing the edges in the overlap network
+		OverlappingNetwork on = pair.getOverlappingNetwork();
+		Set<Edge> oEdges = on.getEdges();
+		assertEquals(1, oEdges.size());
+		assertAnEdge(on, "A", "B", true, "ppi", false, 0.4); 
+	}
+	
+	/**
+	 * JUNIT Test: check whether the example fuzzy network produces correct results with the min operator and cutoff 4 out of 4
+	 */
+	@Test
+	public void testFuzzy_4_max()
+	{
+		FuzzyNetworks2 ex = new FuzzyNetworks2();
+		double weight_cutoff = 0.0;
+		Project p = ex.getProject();
+		int overlap_cutoff = 4;
+		int ID = ex.getTestConfiguration(p, overlap_cutoff);
+
+		new CalculateDiff().calculateOneDifferentialNetwork(p, ID, weight_cutoff, 70, 80, false);
+
+		// Testing that there is exactly one differential network created
+		RunOutput output = p.getOutput(ID);
+		assertNrPairs(output, 1);
+		
+		OutputNetworkPair pair = output.getOutputAsPairs().iterator().next();
+
+		// Testing the edges in the differential network
+		DifferentialNetwork dn = pair.getDifferentialNetwork();
+		Set<Edge> dEdges = dn.getEdges();
+		assertEquals(0, dEdges.size());
+		
+		// Testing the edges in the overlap network
+		OverlappingNetwork on = pair.getOverlappingNetwork();
+		Set<Edge> oEdges = on.getEdges();
+		assertEquals(1, oEdges.size());
+		assertAnEdge(on, "A", "B", true, "ppi", false, 1.2); 
+	}
+	
+	/**
+	 * JUNIT Test: check whether the example fuzzy network produces correct results with the min operator and cutoff 3 out of 4
+	 */
+	@Test
+	public void testFuzzy_3_min()
+	{
+		FuzzyNetworks2 ex = new FuzzyNetworks2();
+		double weight_cutoff = 0.0;
+		Project p = ex.getProject();
+		int overlap_cutoff = 3;
+		int ID = ex.getTestConfiguration(p, overlap_cutoff);
+
+		new CalculateDiff().calculateOneDifferentialNetwork(p, ID, weight_cutoff, 70, 80, true);
+
+		// Testing that there is exactly one differential network created
+		RunOutput output = p.getOutput(ID);
+		assertNrPairs(output, 1);
+		
+		OutputNetworkPair pair = output.getOutputAsPairs().iterator().next();
+
+		// Testing the edges in the differential network
+		DifferentialNetwork dn = pair.getDifferentialNetwork();
+		Set<Edge> dEdges = dn.getEdges();
+		assertEquals(1, dEdges.size());
+		assertAnEdge(dn, "A", "B", true, "decrease_ppi", false, 0.2); 
+		
+		// Testing the edges in the overlap network
+		OverlappingNetwork on = pair.getOverlappingNetwork();
+		Set<Edge> oEdges = on.getEdges();
+		assertEquals(1, oEdges.size());
+		assertAnEdge(on, "A", "B", true, "ppi", false, 0.6); 
+	}
+	
+	/**
+	 * JUNIT Test: check whether the example fuzzy network produces correct results with the min operator and cutoff 3 out of 4
+	 */
+	@Test
+	public void testFuzzy_3_max()
+	{
+		FuzzyNetworks2 ex = new FuzzyNetworks2();
+		double weight_cutoff = 0.0;
+		Project p = ex.getProject();
+		int overlap_cutoff = 3;
+		int ID = ex.getTestConfiguration(p, overlap_cutoff);
+
+		new CalculateDiff().calculateOneDifferentialNetwork(p, ID, weight_cutoff, 70, 80, false);
+
+		// Testing that there is exactly one differential network created
+		RunOutput output = p.getOutput(ID);
+		assertNrPairs(output, 1);
+		
+		OutputNetworkPair pair = output.getOutputAsPairs().iterator().next();
+
+		// Testing the edges in the differential network
+		DifferentialNetwork dn = pair.getDifferentialNetwork();
+		Set<Edge> dEdges = dn.getEdges();
+		assertEquals(1, dEdges.size());
+		assertAnEdge(dn, "A", "B", true, "decrease_ppi", false, 0.2);
+		
+		// Testing the edges in the overlap network
+		OverlappingNetwork on = pair.getOverlappingNetwork();
+		Set<Edge> oEdges = on.getEdges();
+		assertEquals(1, oEdges.size());
+		assertAnEdge(on, "A", "B", true, "ppi", false, 1.2); 
+	}
+	
+	/**
+	 * JUNIT Test: check whether the example fuzzy network produces correct results with the min operator and cutoff 2 out of 4
+	 */
+	@Test
+	public void testFuzzy_2_min()
+	{
+		FuzzyNetworks2 ex = new FuzzyNetworks2();
+		double weight_cutoff = 0.0;
+		Project p = ex.getProject();
+		int overlap_cutoff = 2;
+		int ID = ex.getTestConfiguration(p, overlap_cutoff);
+
+		new CalculateDiff().calculateOneDifferentialNetwork(p, ID, weight_cutoff, 70, 80, true);
+
+		// Testing that there is exactly one differential network created
+		RunOutput output = p.getOutput(ID);
+		assertNrPairs(output, 1);
+		
+		OutputNetworkPair pair = output.getOutputAsPairs().iterator().next();
+
+		// Testing the edges in the differential network
+		DifferentialNetwork dn = pair.getDifferentialNetwork();
+		Set<Edge> dEdges = dn.getEdges();
+		assertEquals(0, dEdges.size());
+		
+		// Testing the edges in the overlap network
+		OverlappingNetwork on = pair.getOverlappingNetwork();
+		Set<Edge> oEdges = on.getEdges();
+		assertEquals(1, oEdges.size());
+		assertAnEdge(on, "A", "B", true, "ppi", false, 0.8); 
+	}
+	
+	/**
+	 * JUNIT Test: check whether the example fuzzy network produces correct results with the min operator and cutoff 3 out of 4
+	 */
+	@Test
+	public void testFuzzy_2_max()
+	{
+		FuzzyNetworks2 ex = new FuzzyNetworks2();
+		double weight_cutoff = 0.0;
+		Project p = ex.getProject();
+		int overlap_cutoff = 2;
+		int ID = ex.getTestConfiguration(p, overlap_cutoff);
+
+		new CalculateDiff().calculateOneDifferentialNetwork(p, ID, weight_cutoff, 70, 80, false);
+
+		// Testing that there is exactly one differential network created
+		RunOutput output = p.getOutput(ID);
+		assertNrPairs(output, 1);
+		
+		OutputNetworkPair pair = output.getOutputAsPairs().iterator().next();
+
+		// Testing the edges in the differential network
+		DifferentialNetwork dn = pair.getDifferentialNetwork();
+		Set<Edge> dEdges = dn.getEdges();
+		assertEquals(0, dEdges.size());
+		
+		// Testing the edges in the overlap network
+		OverlappingNetwork on = pair.getOverlappingNetwork();
+		Set<Edge> oEdges = on.getEdges();
+		assertEquals(1, oEdges.size());
+		assertAnEdge(on, "A", "B", true, "ppi", false, 1.2); 
+	}
+}

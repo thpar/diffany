ddfc461912c4343402f9d0823676ea3a35fde99e
svlandeg <svlandeg@ugent.be>
2014-12-01 09:00:23 +0100

    remove specific R code

--- diffany/src/main/resources/Rcode/NormalizeAffyData.R
+++
@@ -1,8 +0,0 @@
-#source("http://bioconductor.org/biocLite.R");
-#biocLite("affy");
-
-library(affy);
-
-expressionSet <- rma(rawProbeData);
-probesets <- featureNames(expressionSet);
-samples <- sampleNames(expressionSet);
